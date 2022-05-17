be3886cccbc2b2a85b93afda9c7f9e2feb8fa941
IvanTron <99278095+IvanRoger20@users.noreply.github.com>
2022-05-17 09:51:12 -0600

    Add files via upload

--- src/eva3_17_custom_exception_2/EVA3_17_CUSTOM_EXCEPTION_2.java
+++ src/eva3_17_custom_exception_2/EVA3_17_CUSTOM_EXCEPTION_2.java
@@ -16,6 +16,53 @@ public class EVA3_17_CUSTOM_EXCEPTION_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try{
+            Persona persona = new Persona("Juan Perez", -15);
+        }catch (ExcepcionDeRango ex) {
+            ex.printStackTrace();
+        }
     }
+    
+}
+
+class ExcepcionDeRango extends Exception {
+
+    public ExcepcionDeRango() {
+    }
+
+    public ExcepcionDeRango(String string) {
+        super(string);
+    }
+    
+}
+
+class Persona {
+    String Nombre;
+    int Edad;
+
+    public Persona(String Nombre, int Edad) {
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+    
     
 }
